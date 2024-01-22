2a21c5b3e945462208931d40be4688b33e582eac
abaidurrehman <abaidurrehman@gmail.com>
2024-01-22 20:05:49 +0100

    Apply more clean up

--- server/Timelogger/Dto/TimeRegistrationDto.cs
+++ server/Timelogger/Dto/TimeRegistrationDto.cs
@@ -8,7 +8,6 @@ namespace Timelogger.Dto
         public int ProjectId { get; set; }
         public int FreelancerId { get; set; }
         public string TaskDescription { get; set; }
-        public DateTime Date { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
     }
